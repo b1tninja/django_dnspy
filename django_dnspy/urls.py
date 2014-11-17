8eabdaa1d218300b4529e3abbdcf429d4a3812a7
b1tninja <b1tninja@users.noreply.github.com>
2014-11-17 01:08:58 -0800

    Refactored the templates to allow for DetailViews and added some url hrefs

--- django_dnspy/urls.py
+++ django_dnspy/urls.py
@@ -4,13 +4,19 @@ from django.contrib import admin
 from django.conf import settings
 
 from django.views.generic import ListView, TemplateView
+
 from dnspy.models import Name, Record, Question, Query
+from dnspy.views import NameDetailView, QuestionDetailView, QueryDetailView, RecordDetailView
 
 urlpatterns = patterns('',
     url(r'^$', TemplateView.as_view(template_name='index.html'), name='index'),
     url(r'^names/$', ListView.as_view(model=Name,), name='name_list'),
+    url(r'^name/(?P<pk>\d+)/$', NameDetailView.as_view(), name='name'),
     url(r'^questions/$', ListView.as_view(model=Question,), name='question_list'),
+    url(r'^question/(?P<pk>\d+)/$', QuestionDetailView.as_view(), name='question'),
     url(r'^records/$', ListView.as_view(model=Record,), name='record_list'),
+    url(r'^record/(?P<pk>\d+)/$', RecordDetailView.as_view(), name='record'),
     url(r'^queries/$', ListView.as_view(model=Query,), name='query_list'),
+    url(r'^query/(?P<pk>\d+)/$', QueryDetailView.as_view(), name='query'),
     url(r'^admin/', include(admin.site.urls)),
 ) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- dnspy/views.py
+++ dnspy/views.py
@@ -1,3 +1,31 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.views.generic import DetailView
+from dnspy.models import Name, Question, Query, Record
+
+class NameDetailView(DetailView):
+    context_object_name = "name"
+    model = Name
+
+class QuestionDetailView(DetailView):
+    context_object_name = "question"
+    model = Question
+
+class QueryDetailView(DetailView):
+    context_object_name = "query"
+    model = Query
+
+class RecordDetailView(DetailView):
+    context_object_name = "record"
+    model = Record
+
+
+
+
+    # def get_context_data(self, **kwargs):
+    #     # Call the base implementation first to get a context
+    #     context = super(NameDetailView, self).get_context_data(**kwargs)
+    #     # Add in a QuerySet of all the books
+    #     context['book_list'] = Book.objects.all()
+    #     return context
