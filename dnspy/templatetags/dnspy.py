98309de725173f333efcb4a2de08902a6027a46e
b1tninja <b1tninja@users.noreply.github.com>
2015-05-19 21:25:56 -0700

    catch ValueError for unknown record types/classes

--- dnspy/templatetags/dnspy.py
+++ dnspy/templatetags/dnspy.py
@@ -7,16 +7,28 @@ from DNSpy.enums import DnsQClass, DnsRClass, DnsQType, DnsRType
 
 @register.filter
 def rtype(x):
-    return str(DnsRType(x))
+    try:
+        return str(DnsRType(x))
+    except ValueError:
+        return "DnsRType.%d" % x
 
 @register.filter
 def qtype(x):
-    return str(DnsQType(x))
+    try:
+        return str(DnsQType(x))
+    except ValueError:
+        return "DnsQType.%d" % x
 
 @register.filter
 def rclass(x):
-    return str(DnsRClass(x))
+    try:
+        return str(DnsRClass(x))
+    except ValueError:
+        return "DnsRClass.%d" % x
 
 @register.filter
 def qclass(x):
-    return str(DnsQClass(x))
+    try:
+        return str(DnsQClass(x))
+    except ValueError:
+        return "DnsQClass.%d" % x
